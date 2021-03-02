b5e96763a4a4ca777d9a72a10fad422e99ee4fa0
Gregory Vochin <gvochin3@gmail.com>
2021-03-02 09:39:58 -0500

    first commit

---
+++ TipCalc.py
@@ -0,0 +1,38 @@
+#!C:\Users\gvoch\Desktop\TipCalculator\TipCalc.py
+
+"""
+TipCalc.py: Used to calculate a 10, 12, or 15 percent portion of a user entered
+               float,adding that portion to the initial float and then splitting
+               it over an integer provided by the User.
+"""
+
+__author__ = "Gregory Vochin"
+__maintainer__ = "Gregory Vochin"
+__email__ = "gvochin3@gmail.com"
+__status__ = "Development"
+
+print("Welcome to the tip calculator.")
+
+totalBill = input("What was the total bill? ")
+peoplePaying = input("How many people to split the bill? ")
+
+tipPercentages = {"10": 1.10, "12": 1.12, "15": 1.15}
+selectedTip = None
+
+while selectedTip not in tipPercentages:
+    selectedTip = input("What percentage tip would you like to give? 10, 12, or 15? ")
+    print(f"You have selected {selectedTip}%.")
+    if selectedTip in tipPercentages:
+        selectedTip = tipPercentages[selectedTip]
+        break
+    else:
+        print("Whoops! Please choose form the provided values.")
+        continue
+
+tippedBill = float(totalBill) * float(selectedTip)
+roundedTotalBill = round(tippedBill, 2)
+print(f"The total bill is ${roundedTotalBill}")
+
+splitBill = float(roundedTotalBill) / float(peoplePaying)
+roundedSplitBill = round(splitBill, 2)
+print(f"Each patron is due to pay ${roundedSplitBill}")
